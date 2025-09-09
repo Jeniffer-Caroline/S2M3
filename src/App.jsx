5c93c13ccee3c9eaf101302aafa0bb9070cca49c
Jeniffer-Caroline <jeniffer_caroline150@estudante.sesisenai.org.br>
2025-09-09 15:16:53 -0300

    Tela de listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import CadastroProduto from './CadastroProduto'
-
+import Produto from './TelaListagem'
 function App() {
   return (
     <div>
       <CadastroProduto />
+      <Produto />
     </div>
+
   )
 }
 
---
+++ src/TelaListagem.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles/TelaListagem.css';
+
+const Produto = () => {
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/produtos')
+            .then(response => {
+                setProdutos(response.data);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar produtos:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            {produtos.map(produto => (
+                <div key={produto.id} className="card">
+                            <h2>{produto.nome}</h2>
+                            <p>Preço: R$ {produto.preco}</p>
+                            <p>Descrição: {produto.descricao}</p>
+                            <img src={produto.imagemUrl} alt={produto.nome} />
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </div>
+                    ))}
+        </div>
+    );
+};
+
+export default Produto;
